cfd61154a05d20f2b5aeb81b505eee7abd5f3699
Hélio <35930529+Olieh@users.noreply.github.com>
2023-01-21 15:38:30 -0300

    Add files via upload

---
+++ Projeto1/01_sintaxe_basica/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author estud
+ *
+ */
+module sintaxe_basica {
+}
---
+++ Projeto1/02_tipo_inteiro/src/sintaxe_basica/TestaVariaveis.java
@@ -0,0 +1,16 @@
+package sintaxe_basica;
+
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+		System.out.println("Ola novo teste");
+		System.out.println("--------------");
+
+		int idade;
+		idade = 37;
+		System.out.println("idade é " + idade);
+		
+		idade = 30 + 10;
+		System.out.println("idade é " + idade);
+	}
+}
