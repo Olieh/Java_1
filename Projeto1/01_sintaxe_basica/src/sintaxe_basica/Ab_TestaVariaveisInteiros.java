fe8038133a9c6a3299f2cce3772d976e2b6cda7c
Hélio <35930529+Olieh@users.noreply.github.com>
2023-01-22 09:32:37 -0300

    Add files via upload

---
+++ Projeto1/01_sintaxe_basica/src/sintaxe_basica/Ab_TestaVariaveisInteiros.java
@@ -0,0 +1,18 @@
+package sintaxe_basica;
+
+public class Ab_TestaVariaveisInteiros {
+
+	public static void main(String[] args) {
+//		declarando variaavel idade do tipo inteiro
+		int idade;
+//		definindo valor 37 para a variavel idade
+		idade = 37;
+//		imprimindo o valor da variavel idade
+		System.out.println("idade é " + idade);
+		
+//		substituindo o valor da variavel idade pelo resultado da soma de 30 + 10
+		idade = 30 + 10;
+//		imprimindo o novo valor da variavel idade
+		System.out.println("idade é " + idade);
+	}
+}
